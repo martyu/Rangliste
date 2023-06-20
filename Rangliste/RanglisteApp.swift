71b335bc588f553bdf39c37bdf7a230e513f8c9d
marty <marty@martyulrich.com>
2023-06-19 19:24:09 -0700

    Initial Commit

---
+++ Rangliste/RanglisteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RanglisteApp.swift
+//  Rangliste
+//
+//  Created by Marty Ulrich on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct RanglisteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
